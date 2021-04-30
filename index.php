95507c81a703734a468bbdb02297d107c77a035a
cglaz <cezazzr@gmail.com>
2021-04-30 22:29:23 +0200

    Autoloader

--- index.php
+++ index.php
@@ -2,26 +2,33 @@
 
 declare(strict_types=1);
 
-namespace App;
-
-require_once('src/Exception/AppException.php');
-require_once("src/Utils/debug.php");
-require_once("src/Controller.php");
-require_once("src/Request.php");
+spl_autoload_register(function(string $name) {
+    var_dump($name);
+});
 
 use App\Request;
-use App\Exception\AppException;
-use App\Exception\ConfigurationException;
-use Throwable; 
-
-$configuration = require_once("config/config.php");
 
 $request = new Request($_GET, $_POST);
 
+exit('end');
+
+require_once('src/Exception/AppException.php');
+require_once("src/Utils/debug.php");
+require_once("src/Controller/NoteController.php");
+require_once("src/Request.php");
+
+//use App\Request;
+use App\Exception\AppException;
+use App\Exception\ConfigurationException;
+
+$configuration = require_once("config/config.php");
+
+
+
 
 try {
-    Controller::initConfiguration($configuration);
-    (new Controller($request))->run();
+    AbstractController::initConfiguration($configuration);
+    (new NoteController($request))->run();
 } catch (ConfigurationException $e){
     //mail('xxx@xxxx.com', 'Error', $e->getMessage());
     echo '<h1>Wystąpił błąd aplikacji</h1>';
@@ -29,7 +36,7 @@ try {
 } catch (AppException $e) {
     echo "<h1>Wystąpił błąd aplikacji</h1>";
     echo '<h3>'.$e->getMessage().'</h3>';
-} catch (Throwable $e) {
+} catch (\Throwable $e) {
     echo "<h1>Wystąpił błąd aplikacji</h1>";
     dump($e);
 }
--- templates/pages/show.php
+++ templates/pages/show.php
@@ -2,10 +2,10 @@
 <?php $note = $params['note'] ?? null; ?>
 <?php if($note) : ?>
     <ul>
-        <li>Id: <?php echo (int) $note['id'] ?></li>        
-        <li>Tytuł: <?php echo htmlentities($note['title']) ?></li>        
-        <li>Opis: <?php echo htmlentities($note['description']) ?></li>        
-        <li>Data utworzenia: <?php echo htmlentities($note['created']) ?></li>        
+        <li>Id: <?php echo $note['id'] ?></li>        
+        <li>Tytuł: <?php echo $note['title'] ?></li>        
+        <li>Opis: <?php echo $note['description'] ?></li>        
+        <li>Data utworzenia: <?php echo $note['created'] ?></li>        
     </ul>
 <?php else : ?>
 <div>
